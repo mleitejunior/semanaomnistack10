bf5ac40374f48a18fbc75984f4dfb8e7b68222f8
Marcelo Leite Junior <mleitejunior@gmail.com>
2020-01-16 17:47:59 -0300

    Aula 3 completa

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" >
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+          
+        </ul>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+//Componente (função que retorna um conteúdo html, css, js para interface etc.)
+//Quando repetimos código ou precisamos isolar uma parte da aplicação dentro de
+//algo que não infrinja no funcionamento dos outros componentes 
+
+//Propriedade
+//Em html chamava de atributo (class="", title="" etc...)
+//Informações que um componente PAI passa para o componente FILHO
+
+//Estado
+//Informação mantida pelo componente (lembrar do conceito de imutabilidade)
+// { useState } , const [counter, setCounter] = useState(0)
+// setCounter(counter +1)
